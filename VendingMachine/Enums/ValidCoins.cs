709350a1fe82c1bb42dbdd0d3951e41288e21d27
alvin <alvin@DESKTOP-AE8PM2K>
2023-01-18 14:39:31 +0530

    created enums

---
+++ VendingMachine/Enums/ValidCoins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Enums
+{
+    public enum ValidCoins
+    {
+        nickels=5,
+        dimes=10,
+        quarters=25,
+    }
+}
---
+++ VendingMachine/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Models
+{
+    public class Account
+    {
+
+        public decimal Balance { get; set; }
+        public decimal Change { get; set; }
+    }
+}
